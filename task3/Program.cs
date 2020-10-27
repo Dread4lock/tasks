bdc61ed7842ae6785b0871ab090e6fc785e02811
Dread4lock <55984714+Dread4lock@users.noreply.github.com>
2020-10-27 12:25:54 +0300

    task3 solution

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) //Бесконечный цикл
+            { 
+                Console.WriteLine(RPN.Calculate(Console.ReadLine())); //Считываем, и выводим результат
+            }
+        }
+    }
+}
